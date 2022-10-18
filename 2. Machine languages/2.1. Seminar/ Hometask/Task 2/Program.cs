9b0d9841a6c09d0458a41d74c799c06316412f36
Denis <denisalkaev1986@gmail.com>
2022-10-18 18:55:45 +0300

    Added the Task 2

---
+++ 2. Machine languages/2.1. Seminar/ Hometask/Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Clear();
+
+int numbers = 2;
+List<int> numbersList = new List<int>();
+
+for (int i = 0; i < numbers; i++)
+{
+    Console.Write($"Введите {i} число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    numbersList.Add(number);
+}
+int min_num = numbersList[0];
+int max_num = min_num;
+
+for (int i = 1; i < numbers; i++)
+{
+    if (numbersList[i] > min_num)
+    {
+        max_num = numbersList[i];
+    }
+    else
+    {
+        min_num = numbersList[i];
+    }
+}
+
+Console.WriteLine($"Максимально число {max_num}, Минимальноче число {min_num}");
