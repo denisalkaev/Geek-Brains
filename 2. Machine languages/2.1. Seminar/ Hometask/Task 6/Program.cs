aff2d55e1abb8998dfea831c1625ba77c0b4d04c
Denis <denisalkaev1986@gmail.com>
2022-10-18 22:35:12 +0300

    Added Task 6

---
+++ 2. Machine languages/2.1. Seminar/ Hometask/Task 6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int i = number % 2;
+string answer;
+
+if (i == 0)
+{
+    answer = "Четное число";
+}
+else
+{
+    answer = "Нечетное число";
+}
+
+Console.WriteLine(answer);
