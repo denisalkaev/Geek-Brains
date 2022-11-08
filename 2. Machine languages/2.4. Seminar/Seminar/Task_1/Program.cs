3480caa556bf66d6eebbea2db1cea9ede40fd244
Denis <denisalkaev1986@gmail.com>
2022-11-08 23:39:53 +0300

    Added seminar 2.4

---
+++ 2. Machine languages/2.4. Seminar/Seminar/Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Найти произведение чисел от 1 до N
+
+// Console.Clear();
+// int GetMult(int value)
+// {
+//     int mult = 1;
+//     for (int i = 1; i <= value; i++)
+//     {
+//         mult *= i;
+//     }
+//     return mult;
+// }
+
+// Console.Write("Input value: ");
+// int N = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Произведение числе равно {GetMult(N)}");
+
+// Написать программу по выводу случайнного массива из 8 элементов
+
+Console.Clear();
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+// void PrintArray(int[] array)
+// {
+//     for (int i=0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]);
+//     }
+// }
+
+int[] array = new int[8];
+FillArray(array);
+// PrintArray(array);
+Console.WriteLine(string.Join(",", array));
