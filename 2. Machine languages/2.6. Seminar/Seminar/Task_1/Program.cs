f98d48e86b64f351c78503293588203b20b464a1
Denis <denisalkaev1986@gmail.com>
2022-11-15 22:44:22 +0300

    Added seminar

---
+++ 2. Machine languages/2.6. Seminar/Seminar/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Являются ли введеные числа сторонами треугольника
+
+Console.Clear();
+
+bool CheckTriangle(int a, int b, int c)
+{
+    return (a < b + c && b < a + c && c < a + b);
+}
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Triangle with sides {a} {b} {c} " + (CheckTriangle(a, b, a) ? "is" : "is not") + " triangle");
---
+++ 2. Machine languages/2.6. Seminar/Seminar/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Перевести десятичное число в двоичное
+
+Console.Clear();
+
+int GetBinarNumber(int number)
+{
+    string result = String.Empty;
+    if (number == 0) return 0;
+    while (number > 0)
+    {
+        result += (number % 2).ToString();
+        number /= 2;
+    }
+    return int.Parse(result); //  Необходимо перевести в число полученное значение, чтобы можно было осуществлять с ним действия
+}
+
+Console.Write("Input number: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetBinarNumber(num));
