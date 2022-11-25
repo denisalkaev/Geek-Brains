f5fd7317f5fc6fa154b4fb32f2661a740c31687d
Denis <denisalkaev1986@gmail.com>
2022-11-25 12:31:24 +0300

    Added hometask

---
+++ 2. Machine languages/2.8. Seminar/Hometask/Task_star/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача со звездочкой. Написать функцию Sqrt(x) - квадратного корня, 
+// которая принимает на вход целочисленное значение x и возвращает целую часть квадратного корня от введенного числа.
+
+Console.Clear();
+
+int MySqrt(int number)
+{
+    int i = 1;
+    while (number >= i * i) i++;
+    return i-1;
+}
+
+Console.Write("Input number: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Integer of square root = {MySqrt(N)}");
